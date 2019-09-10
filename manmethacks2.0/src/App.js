6593486cb9fce28dcd864c49d7d42fc27a3e44eb
Thomas Misson <11tmisson@gmail.com>
2019-09-10 18:38:49 +0100

    Defined basic structure to build modules out from

--- manmethacks2.0/src/App.js
+++ manmethacks2.0/src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import TeamPanel from './Components/TeamPanel';
 import './App.css';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body>
+      <header>
+  
       </header>
-    </div>
+
+      <main>
+        <TeamPanel></TeamPanel>
+      </main>
+
+      <footer>
+
+      </footer>
+    </body>
   );
 }
 
