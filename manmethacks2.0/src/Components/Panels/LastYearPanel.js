6884d6734a23b1d2dd67e8aba1a448acab49f181
Thomas Misson <11tmisson@gmail.com>
2019-09-22 20:47:48 +0100

    Added content

--- manmethacks2.0/src/Components/Panels/LastYearPanel.js
+++ manmethacks2.0/src/Components/Panels/LastYearPanel.js
@@ -16,13 +16,13 @@ export default class LastYear extends Component {
                 <h2>Version Control</h2>
                 <h3>ManMetMacks 1.0</h3>
                 <br/>
-                <p>Last year at ManMetHacks...</p>
-                <br/><br/><br/>
+                <p>Last year was our first edition of our 24 hour student hackathon in Manchester Metropolitan University. We hosted around 100 student hackers from all over Europe. Together, they met to code, design and build innovative technological solutions.
+                </p>
                 <h3>Our sponsors</h3>
                 <div className="sponsorWrapper">
                 {
                     this.state.sponsorImages.map((image) =>
-                        <img src={image} height="90" width="200"/>
+                        <img key={image} src={image} height="90" width="200"/>
                     )
                 }
                 </div>
--- manmethacks2.0/src/Components/Panels/SponsorsPanel.js
+++ manmethacks2.0/src/Components/Panels/SponsorsPanel.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import load from '../../Media/loading.gif'
 
 export default class Sponsors extends Component {
     render() {
         return (
             <section>
                 <h2>This years sponsors</h2>
-                <p>Tbc...</p>
+                <img className="loading" alt ="loading circle" src={load}></img>
             </section>
         )
     }
