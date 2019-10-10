9fdf802d7bc13176648b6279c535741a59809f52
Alexandra Jones <alexandra.e@hotmail.co.uk>
2019-10-10 10:53:38 +0100

    Sorted images sizes so they can be defined in state

--- manmethacks2.0/src/Components/Panels/LastYearPanel.js
+++ manmethacks2.0/src/Components/Panels/LastYearPanel.js
@@ -6,14 +6,36 @@ import wauk from '../../Media/wauk-logo.png'
 
 export default class LastYear extends Component {
 
-    state={
-        sponsorImages:[foundry,github,stickermule,wauk]
+    state = {
+        sponsorImages: [{
+                image: foundry,
+                width: 299,
+                height: 54,
+            },
+            {
+                image: github,
+                width: 249,
+                height: 140,
+            },
+            {
+                image: stickermule,
+                width: 240,
+                height: 140,
+            },
+            {
+                image: wauk,
+                width: 398,
+                height: 140,
+            }
+        ]
     }
 
     render() {
+    
         return (
             <section>
                 <h2>Version Control</h2>
+                <br/>
                 <h3>ManMetMacks 1.0</h3>
                 <br/>
                 <p>Last year at ManMetHacks...</p>
@@ -22,7 +44,7 @@ export default class LastYear extends Component {
                 <div className="sponsorWrapper">
                 {
                     this.state.sponsorImages.map((image) =>
-                        <img src={image} height="90" width="200"/>
+                        <img src={image.image} height={image.height} width={image.width}/>
                     )
                 }
                 </div>
